dfcfc5b633d8edad2359aa996e0d756b796a0996
Joshua Darby <joshuajdarby@gmail.com>
2021-10-25 04:08:49 -0500

    created files for constructor to pass props to result cards

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import * as ReactDOM from "react-dom";
 import { createGlobalStyle } from "styled-components";
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';import App from "./App";
 import { globalStyle } from "./styles";
 const GlobalStyle = createGlobalStyle`
   ${globalStyle}
